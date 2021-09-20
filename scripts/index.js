4b157812b8159b23228658ccaffb36adc6a24e15
MedusCode <WoWandreev@gmail.com>
2021-09-20 22:50:08 +0300

    feat: add full-photo popup / refactor: initail-photo-cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const popupEditForm = document.querySelector('#popup-edit-form');
 const popupAddForm = document.querySelector('#popup-add-form');
+const popupFullPhoto = document.querySelector('#popup-full-photo');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const editForm = popupEditForm.querySelector('.form');
@@ -39,6 +40,7 @@ const initialCards = [
   }
 ];
 
+
 function EditFormSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -51,6 +53,11 @@ function AddFormSubmitHandler (evt) {
   const newPhotoCard = photoCardTemplate.querySelector('.card').cloneNode(true);
   newPhotoCard.querySelector('.card__photo').src = photoLinkInput.value;
   newPhotoCard.querySelector('.card__photo-name').textContent = photoNameInput.value;
+  newPhotoCard.querySelector('.card__photo').addEventListener('click', (event) => {
+    popupFullPhoto.querySelector('.full-photo__photo').src = event.target.src;
+    popupFullPhoto.querySelector('.full-photo__name').textContent = event.target.parentElement.querySelector('.card__photo-name').textContent;
+    popupFullPhoto.classList.add('popup_opened');
+  })
   newPhotoCard.querySelector('.card__like-button').addEventListener('click', (event) => {
     event.target.classList.toggle('card__like-button_active');
   })
@@ -63,18 +70,18 @@ function AddFormSubmitHandler (evt) {
   popupAddForm.classList.remove('popup_opened');
 }
 
+editForm.addEventListener('submit', EditFormSubmitHandler);
 
-initialCards.forEach((element) => {
-  const newPhotoCard = photoCardTemplate.querySelector('.card').cloneNode(true);
-  newPhotoCard.querySelector('.card__photo').src = element.link;
-  newPhotoCard.querySelector('.card__photo-name').textContent = element.name;
-  newPhotoCard.querySelector('.card__like-button').addEventListener('click', (event) => {
-    event.target.classList.toggle('card__like-button_active');
-  })
-  newPhotoCard.querySelector('.card__trash-button').addEventListener('click', (event) => {
-    event.target.closest('.card').remove();
-  })
-  cardsList.prepend(newPhotoCard);
+addForm.addEventListener('submit', AddFormSubmitHandler);
+
+editButton.addEventListener('click', () => {
+  nameInput.value = profileName.textContent;
+  addInfoInput.value = profileAddInfo.textContent;
+  popupEditForm.classList.add('popup_opened');
+})
+
+addButton.addEventListener('click', () => {
+  popupAddForm.classList.add('popup_opened');
 })
 
 document.querySelectorAll('.popup__close-button').forEach((CloseButton) => {
@@ -83,18 +90,10 @@ document.querySelectorAll('.popup__close-button').forEach((CloseButton) => {
   })
 });
 
-editButton.addEventListener('click', () => {
-  popupEditForm.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  addInfoInput.value = profileAddInfo.textContent;
+initialCards.forEach((element) => {
+  photoLinkInput.value = element.link;
+  photoNameInput.value = element.name;
+  addForm.querySelector('.form__save-button').click();
 })
-
-addButton.addEventListener('click', () => {
-  popupAddForm.classList.add('popup_opened');
-})
-
-editForm.addEventListener('submit', EditFormSubmitHandler);
-
-addForm.addEventListener('submit', AddFormSubmitHandler);
 
 
